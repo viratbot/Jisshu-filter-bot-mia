04d7386989f1ef54ca2bd5b57acc5370b8486b44
viratbot <virat09hdj@gmail.com>
2025-03-01 07:58:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 #----------- There will be channel id add in all these ---------
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002354089595'))  # set shortner log channel
 DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0')) # The movie you upload in it will be deleted from the bot.
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002354089595'))
+LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002459987450'))
 auth_channel = environ.get('AUTH_CHANNEL', '-1002354089595')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002286347766'))
 request_channel = environ.get('REQUEST_CHANNEL', '0') # If anyone sends a request message to your bot, you will get it in this channel.
